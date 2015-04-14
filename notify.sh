29c11d973fc31203df57263324ef636d90f6b119
Blueowner <blueowner1@gmail.com>
2015-04-14 19:55:01 +0200

    First commit.

---
+++ notify.sh
@@ -0,0 +1,28 @@
+#/!bin/bash
+
+while [[ $# > 1 ]]
+
+do
+  key="$1"
+  
+  case $key in
+       -t|--title)
+       TITLE="$2"
+       shift
+       ;;
+       -s|--subtitle)
+       SUBTITLE="$2"
+       shift
+       ;;
+       -b|--body)
+       BODY="$2"
+       shift
+       ;;
+       *)
+       # unknown option
+       ;;
+  esac
+  shift
+done
+
+osascript -e "display notification \"$BODY\" with title \"$TITLE\" subtitle \"$SUBTITLE\""
